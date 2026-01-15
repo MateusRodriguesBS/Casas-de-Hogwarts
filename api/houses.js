96fd2a20664aa6df10657a6f72c9c0f24b9e03c5
MateusRodriguesBS <mateus_zatch123@yahoo.com.br>
2026-01-15 13:17:55 -0300

    fazendo rodar no vercel 3

---
+++ api/houses.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://potterhead-api.vercel.app/api/houses');
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao buscar casas' });
+  }
+}
