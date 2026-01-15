fc8e3292599af9959c9cbeda932edcdc09235c6a
MateusRodriguesBS <mateus_zatch123@yahoo.com.br>
2026-01-15 13:06:20 -0300

    ajustando para o vercel

--- src/api/houses/[houseId].js
+++ src/api/houses/[houseId].js
@@ -1,12 +1,13 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
+  const { houseId } = req.query;
   try {
-    const response = await fetch('https://potterhead-api.vercel.app/api/houses');
+    const response = await fetch(`https://potterhead-api.vercel.app/api/houses/${houseId}`);
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Erro ao buscar casas' });
+    res.status(500).json({ error: `Erro ao buscar detalhes da casa ${houseId}` });
   }
 }
